b43daa90c5c5474cd0dce743186026f39be7f155
Debasish Mandal <76836363+devDebasishMandal@users.noreply.github.com>
2024-02-16 17:56:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+let table=document.getElementById("sampleTable");
 function insert_Row() {
     //Write your code here
-  
-  
+  let row=1;
+	let col=1;
+	table.innerHTML+=`<tr><td>New Cell1</td>
+	<td>New Cell2</td></tr> `;
 }
