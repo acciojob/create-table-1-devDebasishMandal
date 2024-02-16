fca7c146ecc293d94a5b8b22655f1582f2d385b5
Debasish Mandal <76836363+devDebasishMandal@users.noreply.github.com>
2024-02-16 20:50:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-// let table=document.getElementById("sampleTable");
+let table=document.getElementById("sampleTable");
 function insert_Row() {
- //    let row =document.createElement("tr");
+    let row =document.createElement("tr");
 	
-	// row.innerHTML=`<td>New Cell1</td>
-	// <td>New Cell2</td>`
-	// table.prepend(row);
-	  var table = document.getElementById("sampleTable");
+	row.innerHTML=`<td>New Cell1</td>
+	<td>New Cell2</td>`
+	table.prepend(row);
+	  // var table = document.getElementById("sampleTable");
 
-    // Insert a new row at the top (index 0) of the table
-    var newRow = table.insertRow(0);
+    // // Insert a new row at the top (index 0) of the table
+    // var newRow = table.insertRow(0);
 
-    // Insert cells into the new row
-    var leftCell = newRow.insertCell(0);
-    var rightCell = newRow.insertCell(1);
+    // // Insert cells into the new row
+    // var leftCell = newRow.insertCell(0);
+    // var rightCell = newRow.insertCell(1);
 
-    // Set the content of the cells
-    leftCell.innerHTML = "New Cell1";
-    rightCell.innerHTML = "New Cell2";
+    // // Set the content of the cells
+    // leftCell.innerHTML = "New Cell1";
+    // rightCell.innerHTML = "New Cell2";
 	
 }
